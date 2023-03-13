53a2b50737ad225ef7acf97e872fd71d28835e21
Bao Hoang <hoangquangbao.developer@gmail.com>
2023-03-13 16:46:36 +0700

    Hiding Map Features with Styling - using a JSON file

--- MapSwiftUI/MapViewController.swift
+++ MapSwiftUI/MapViewController.swift
@@ -60,6 +60,18 @@ class MapViewController: UIViewController, GMSMapViewDelegate {
             NSLog("One or more of the map styles failed to load. \(error)")
         }
         
+        // Hiding Map Features with Styling - using a JSON file
+        do {
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find style.json")
+            }
+        } catch {
+            NSLog("One or more of the map styles failed to load. \(error)")
+        }
+        
         mapView.delegate = self
         self.view = mapView
     }
